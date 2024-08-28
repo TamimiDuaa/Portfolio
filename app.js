c1c477f38984b28bceba27fcd26929c59f44d415
TamimiDuaa <tamimiduaa18@gmail.com>
2024-08-27 20:00:33 -0400

    fix the age and remove social media links

---
+++ app.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function( ){
+    $('.toggle-collapse').click(function(){
+    $('.navbar-ul').toggleClass("collapse");
+    $('.nav-item').toggleClass("nav-item-col");
+    })
+})
+function install(){
+    window.open("https://drive.google.com/file/d/1bnyahTK3PeX25pSh-2LLwEh6JGOjylPh/view?usp=sharing");
+}
+const scrollAnimations = document.querySelectorAll(".sk");
+
+function handleScroll() {
+    const scrollY = window.scrollY;
+    const windowHeight = window.innerHeight;
+
+    scrollAnimations.forEach((element) => {
+        const elementOffset = element.getBoundingClientRect().top;
+
+        if (elementOffset < windowHeight ) {
+            element.classList.add("animate");
+        }
+    });
+}
+
+window.addEventListener("scroll", handleScroll);
